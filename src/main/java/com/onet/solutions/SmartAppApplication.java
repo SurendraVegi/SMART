1b9063fb00e7158f599c95ad8e057c3b26dd11f1
Surendra Vegi <surendra.vegi@gmail.com>
2019-09-11 11:54:30 +0530

    test

--- src/main/java/com/onet/solutions/SmartAppApplication.java
+++ src/main/java/com/onet/solutions/SmartAppApplication.java
@@ -11,6 +11,7 @@ public class SmartAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SmartAppApplication.class, args);
 	}
+	//test
 	@Bean
 	public SpringDataDialect springDataDialect() {
 		return new SpringDataDialect();
