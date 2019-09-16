da629d96ffba8ff648b35e27ac710208aa7a389e
Surendra Vegi <surendra.vegi@gmail.com>
2019-09-16 16:03:58 +0530

    log in app

--- src/main/java/com/onet/solutions/SmartAppApplication.java
+++ src/main/java/com/onet/solutions/SmartAppApplication.java
@@ -1,15 +1,23 @@
 package com.onet.solutions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.thymeleaf.dialect.springdata.SpringDataDialect;
 
+import com.onet.solutions.controller.AddressController;
+
 @SpringBootApplication
 public class SmartAppApplication {
+	
+	public static final Logger log = LoggerFactory.getLogger(AddressController.class);
 
 	public static void main(String[] args) {
 		// test
+		
+		 log.info("info:smart app first log::");
 		SpringApplication.run(SmartAppApplication.class, args);
 	}
 
